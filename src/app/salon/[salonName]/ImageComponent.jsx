c6af0c3a325688c21471dfecb871512a3ad879a0
Vishnu Jangid <103036831+vj2303@users.noreply.github.com>
2024-09-08 15:53:55 +0530

    Update ImageComponent.jsx

--- src/app/salon/[salonName]/ImageComponent.jsx
+++ src/app/salon/[salonName]/ImageComponent.jsx
@@ -6,6 +6,6 @@ import { useState } from "react";
 export default function ImageComponent({ imgUri }) {
     const [fallbackUri, setFallbackUri] = useState(null)
     return (
-        <Image src={fallbackUri?fallbackUri:imgUri} onError={() => setFallbackUri("/luzo_partners.jpg")} alt='img' width={2200} height={200} className="col-12 w-full rounded-4 aspect-w-16 aspect-h-9 h-[380px] object-fill" />
+        <Image src={fallbackUri?fallbackUri:imgUri} onError={() => setFallbackUri("/luzo_partners.jpg")} alt='img' width={2200} height={200} className="col-12 w-full rounded-4 aspect-w-16 aspect-h-9 sm:h-[380px] object-fill" />
     )
 }
